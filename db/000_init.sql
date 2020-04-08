cc1ae938893462eefc6f19c46cb0f718f65904a8
firedial <42674772+firedial@users.noreply.github.com>
2020-04-08 23:19:52 +0900

    db のテーブル名とカラム名の変更

--- db/000_init.sql
+++ db/000_init.sql
@@ -2,127 +2,127 @@ CREATE DATABASE IF NOT EXISTS midas CHARACTER SET utf8mb4 COLLATE utf8mb4_bin;
 USE midas;
 
 
--- attribute_category
-CREATE TABLE IF NOT EXISTS kind_category
+-- m_attribute_category
+CREATE TABLE IF NOT EXISTS m_kind_category
 (
     kind_category_id INT NOT NULL AUTO_INCREMENT PRIMARY KEY UNIQUE,  
-    name VARCHAR(20) NOT NULL UNIQUE,
-    description VARCHAR(20) NOT NULL,
-    INDEX(name)
+    kind_category_name VARCHAR(20) NOT NULL UNIQUE,
+    kind_category_description VARCHAR(20) NOT NULL,
+    INDEX(kind_category_name)
 ) ENGINE=InnoDB DEFAULT CHARACTER SET=utf8mb4 COLLATE=utf8mb4_bin;
 
-CREATE TABLE IF NOT EXISTS purpose_category
+CREATE TABLE IF NOT EXISTS m_purpose_category
 (
     purpose_category_id INT NOT NULL AUTO_INCREMENT PRIMARY KEY UNIQUE,  
-    name VARCHAR(20) NOT NULL UNIQUE,
-    description VARCHAR(20) NOT NULL,
-    INDEX(name)
+    purpose_category_name VARCHAR(20) NOT NULL UNIQUE,
+    purpose_category_description VARCHAR(20) NOT NULL,
+    INDEX(purpose_category_name)
 ) ENGINE=InnoDB DEFAULT CHARACTER SET=utf8mb4 COLLATE=utf8mb4_bin;
 
-CREATE TABLE IF NOT EXISTS place_category
+CREATE TABLE IF NOT EXISTS m_place_category
 (
     place_category_id INT NOT NULL AUTO_INCREMENT PRIMARY KEY UNIQUE,  
-    name VARCHAR(20) NOT NULL UNIQUE,
-    description VARCHAR(20) NOT NULL,
-    INDEX(name)
+    place_category_name VARCHAR(20) NOT NULL UNIQUE,
+    place_category_description VARCHAR(20) NOT NULL,
+    INDEX(place_category_name)
 ) ENGINE=InnoDB DEFAULT CHARACTER SET=utf8mb4 COLLATE=utf8mb4_bin;
 
--- attribute_group
-CREATE TABLE IF NOT EXISTS kind_group
+-- m_attribute_group
+CREATE TABLE IF NOT EXISTS m_kind_group
 (
     kind_group_id INT NOT NULL AUTO_INCREMENT PRIMARY KEY UNIQUE,  
-    name VARCHAR(20) NOT NULL UNIQUE,
-    description VARCHAR(20) NOT NULL,
-    INDEX(name)
+    kind_group_name VARCHAR(20) NOT NULL UNIQUE,
+    kind_group_description VARCHAR(20) NOT NULL,
+    INDEX(kind_group_name)
 ) ENGINE=InnoDB DEFAULT CHARACTER SET=utf8mb4 COLLATE=utf8mb4_bin;
 
-CREATE TABLE IF NOT EXISTS purpose_group
+CREATE TABLE IF NOT EXISTS m_purpose_group
 (
     purpose_group_id INT NOT NULL AUTO_INCREMENT PRIMARY KEY UNIQUE,  
-    name VARCHAR(20) NOT NULL UNIQUE,
-    description VARCHAR(20) NOT NULL,
-    INDEX(name)
+    purpose_group_name VARCHAR(20) NOT NULL UNIQUE,
+    purpose_group_description VARCHAR(20) NOT NULL,
+    INDEX(purpose_group_name)
 ) ENGINE=InnoDB DEFAULT CHARACTER SET=utf8mb4 COLLATE=utf8mb4_bin;
 
-CREATE TABLE IF NOT EXISTS place_group
+CREATE TABLE IF NOT EXISTS m_place_group
 (
     place_group_id INT NOT NULL AUTO_INCREMENT PRIMARY KEY UNIQUE,  
-    name VARCHAR(20) NOT NULL UNIQUE,
-    description VARCHAR(20) NOT NULL,
-    INDEX(name)
+    place_group_name VARCHAR(20) NOT NULL UNIQUE,
+    place_group_description VARCHAR(20) NOT NULL,
+    INDEX(place_group_name)
 ) ENGINE=InnoDB DEFAULT CHARACTER SET=utf8mb4 COLLATE=utf8mb4_bin;
 
--- attribute
-CREATE TABLE IF NOT EXISTS kind
+-- m_attribute
+CREATE TABLE IF NOT EXISTS m_kind
 (
     kind_id INT NOT NULL AUTO_INCREMENT PRIMARY KEY UNIQUE,  
-    name VARCHAR(20) NOT NULL UNIQUE,
-    description VARCHAR(20) NOT NULL,
+    kind_name VARCHAR(20) NOT NULL UNIQUE,
+    kind_description VARCHAR(20) NOT NULL,
 	kind_group_id INT NOT NULL,
-    INDEX(name),
+    INDEX(kind_name),
     INDEX(kind_group_id),
-    FOREIGN KEY (kind_group_id) REFERENCES kind_group(kind_group_id)
+    FOREIGN KEY (kind_group_id) REFERENCES m_kind_group(kind_group_id)
 ) ENGINE=InnoDB DEFAULT CHARACTER SET=utf8mb4 COLLATE=utf8mb4_bin;
 
-CREATE TABLE IF NOT EXISTS purpose
+CREATE TABLE IF NOT EXISTS m_purpose
 (
     purpose_id INT NOT NULL AUTO_INCREMENT PRIMARY KEY UNIQUE,  
-    name VARCHAR(20) NOT NULL UNIQUE,
-    description VARCHAR(20) NOT NULL,
+    purpose_name VARCHAR(20) NOT NULL UNIQUE,
+    purpose_description VARCHAR(20) NOT NULL,
 	purpose_group_id INT NOT NULL,
-    INDEX(name),
+    INDEX(purpose_name),
     INDEX(purpose_group_id),
-    FOREIGN KEY (purpose_group_id) REFERENCES purpose_group(purpose_group_id)
+    FOREIGN KEY (purpose_group_id) REFERENCES m_purpose_group(purpose_group_id)
 ) ENGINE=InnoDB DEFAULT CHARACTER SET=utf8mb4 COLLATE=utf8mb4_bin;
 
-CREATE TABLE IF NOT EXISTS place
+CREATE TABLE IF NOT EXISTS m_place
 (
     place_id INT NOT NULL AUTO_INCREMENT PRIMARY KEY UNIQUE,  
-    name VARCHAR(20) NOT NULL UNIQUE,
-    description VARCHAR(20) NOT NULL,
+    place_name VARCHAR(20) NOT NULL UNIQUE,
+    place_description VARCHAR(20) NOT NULL,
 	place_group_id INT NOT NULL,
-    INDEX(name),
+    INDEX(place_name),
     INDEX(place_group_id),
-    FOREIGN KEY (place_group_id) REFERENCES place_group(place_group_id)
+    FOREIGN KEY (place_group_id) REFERENCES m_place_group(place_group_id)
 ) ENGINE=InnoDB DEFAULT CHARACTER SET=utf8mb4 COLLATE=utf8mb4_bin;
 
 
--- attribute_category_relation
-CREATE TABLE IF NOT EXISTS kind_category_relation
+-- m_attribute_category_relation
+CREATE TABLE IF NOT EXISTS m_kind_category_relation
 (
     kind_category_id INT NOT NULL,  
     kind_id INT NOT NULL,  
     INDEX(kind_category_id, kind_id),
     INDEX(kind_id, kind_category_id),
     PRIMARY KEY(kind_category_id, kind_id),
-    FOREIGN KEY (kind_category_id) REFERENCES kind_category(kind_category_id),
-    FOREIGN KEY (kind_id) REFERENCES kind(kind_id)
+    FOREIGN KEY (kind_category_id) REFERENCES m_kind_category(kind_category_id),
+    FOREIGN KEY (kind_id) REFERENCES m_kind(kind_id)
 ) ENGINE=InnoDB DEFAULT CHARACTER SET=utf8mb4 COLLATE=utf8mb4_bin;
 
-CREATE TABLE IF NOT EXISTS purpose_category_relation
+CREATE TABLE IF NOT EXISTS m_purpose_category_relation
 (
     purpose_category_id INT NOT NULL,  
     purpose_id INT NOT NULL,  
     INDEX(purpose_category_id, purpose_id),
     INDEX(purpose_id, purpose_category_id),
     PRIMARY KEY(purpose_category_id, purpose_id),
-    FOREIGN KEY (purpose_category_id) REFERENCES purpose_category(purpose_category_id),
-    FOREIGN KEY (purpose_id) REFERENCES purpose(purpose_id)
+    FOREIGN KEY (purpose_category_id) REFERENCES m_purpose_category(purpose_category_id),
+    FOREIGN KEY (purpose_id) REFERENCES m_purpose(purpose_id)
 ) ENGINE=InnoDB DEFAULT CHARACTER SET=utf8mb4 COLLATE=utf8mb4_bin;
 
-CREATE TABLE IF NOT EXISTS place_category_relation
+CREATE TABLE IF NOT EXISTS m_place_category_relation
 (
     place_category_id INT NOT NULL,  
     place_id INT NOT NULL,  
     INDEX(place_category_id, place_id),
     INDEX(place_id, place_category_id),
     PRIMARY KEY(place_category_id, place_id),
-    FOREIGN KEY (place_category_id) REFERENCES place_category(place_category_id),
-    FOREIGN KEY (place_id) REFERENCES place(place_id)
+    FOREIGN KEY (place_category_id) REFERENCES m_place_category(place_category_id),
+    FOREIGN KEY (place_id) REFERENCES m_place(place_id)
 ) ENGINE=InnoDB DEFAULT CHARACTER SET=utf8mb4 COLLATE=utf8mb4_bin;
 
--- balance
-CREATE TABLE IF NOT EXISTS balance
+-- m_balance
+CREATE TABLE IF NOT EXISTS m_balance
 (
     balance_id INT NOT NULL AUTO_INCREMENT PRIMARY KEY UNIQUE,  
     amount INT NOT NULL,
@@ -135,7 +135,7 @@ CREATE TABLE IF NOT EXISTS balance
     INDEX(purpose_id),
     INDEX(place_id),
     INDEX(date),
-    FOREIGN KEY (kind_id) REFERENCES kind(kind_id),
-    FOREIGN KEY (purpose_id) REFERENCES purpose(purpose_id),
-    FOREIGN KEY (place_id) REFERENCES place(place_id)
+    FOREIGN KEY (kind_id) REFERENCES m_kind(kind_id),
+    FOREIGN KEY (purpose_id) REFERENCES m_purpose(purpose_id),
+    FOREIGN KEY (place_id) REFERENCES m_place(place_id)
 ) ENGINE=InnoDB DEFAULT CHARACTER SET=utf8mb4 COLLATE=utf8mb4_bin;
--- specification/db.md
+++ specification/db.md
@@ -7,25 +7,25 @@
 
 |テーブル名|説明|
 |---|---|
-|balance|収支を管理するテーブル|
-|kind|種別のマスタテーブル|
-|purpose|目的のマスタテーブル|
-|place|場所のマスタテーブル|
-|kind_group|種別グループのマスタテーブル|
-|purpose_group|目的グループのマスタテーブル|
-|place_group|場所グループのマスタテーブル|
-|kind_category_relation|種別カテゴリーの関係テーブル|
-|purpose_category_relation|目的カテゴリーの関係テーブル|
-|place_category_relation|場所カテゴリーの関係テーブル|
-|kind_category|種別カテゴリーのマスタテーブル|
-|purpose_category|目的カテゴリーのマスタテーブル|
-|place_category|場所カテゴリーのマスタテーブル|
+|m_balance|収支を管理するテーブル|
+|m_kind|種別のマスタテーブル|
+|m_purpose|目的のマスタテーブル|
+|m_place|場所のマスタテーブル|
+|m_kind_group|種別グループのマスタテーブル|
+|m_purpose_group|目的グループのマスタテーブル|
+|m_place_group|場所グループのマスタテーブル|
+|m_kind_category_relation|種別カテゴリーの関係テーブル|
+|m_purpose_category_relation|目的カテゴリーの関係テーブル|
+|m_place_category_relation|場所カテゴリーの関係テーブル|
+|m_kind_category|種別カテゴリーのマスタテーブル|
+|m_purpose_category|目的カテゴリーのマスタテーブル|
+|m_place_category|場所カテゴリーのマスタテーブル|
 
 ※ kind, purpose, place をまとめて attribute (属性) という (x_group, x_category_relation, x_category も同様)
 
 ## 各テーブルの構成
 
-### balance
+### m_balance
 
 |カラム名|型|補足|インデックス|外部キー|説明|
 |---|---|---|---|---|---|
@@ -37,28 +37,28 @@
 |place_id|INT|NOT NULL|あり|place.place_id|場所|
 |date|DATE|NOT NULL|あり||収支が発生した日付|
 
-### attribute
+### m_attribute
 
 |カラム名|型|補足|インデックス|外部キー|説明|
 |---|---|---|---|---|---|
 |attribute_id|INT|PRIMARY KEY|あり||主キー|
-|name|VARCHAR(20)|NOT NULL, UNIQUE|あり||識別するための名前|
-|description|VARCHAR(20)|NOT NULL|なし||概要|
+|attribute_name|VARCHAR(20)|NOT NULL, UNIQUE|あり||識別するための名前|
+|attribute_description|VARCHAR(20)|NOT NULL|なし||概要|
 |attribute_group_id|INT|NOT NULL|あり|attribute_group.attribute_group_id|属性グループ名|
 
 ※ attribute の部分は kind, purpose, place のどれかに読み替えること
 
-### attribute_group
+### m_attribute_group
 
 |カラム名|型|補足|インデックス|外部キー|説明|
 |---|---|---|---|---|---|
 |attribute_group_id|INT|PRIMARY KEY|あり||主キー|
-|name|VARCHAR(20)|NOT NULL, UNIQUE|あり||識別するための名前|
-|description|VARCHAR(20)|NOT NULL|なし||概要|
+|attribute_group_name|VARCHAR(20)|NOT NULL, UNIQUE|あり||識別するための名前|
+|attribute_group_description|VARCHAR(20)|NOT NULL|なし||概要|
 
 ※ attribute の部分は kind, purpose, place のどれかに読み替えること
 
-### attribute_category_relation
+### m_attribute_category_relation
 
 |カラム名|型|補足|インデックス|外部キー|説明|
 |---|---|---|---|---|---|
@@ -69,13 +69,13 @@
 
 ※ attribute の部分は kind, purpose, place のどれかに読み替えること
 
-### attribute_category
+### m_attribute_category
 
 |カラム名|型|補足|インデックス|外部キー|説明|
 |---|---|---|---|---|---|
 |attribute_category_id|INT|PRIMARY KEY|あり||主キー|
-|name|VARCHAR(20)|NOT NULL, UNIQUE|あり||識別するための名前|
-|description|VARCHAR(20)|NOT NULL|なし||概要|
+|attribute_category_name|VARCHAR(20)|NOT NULL, UNIQUE|あり||識別するための名前|
+|attribute_category_description|VARCHAR(20)|NOT NULL|なし||概要|
 
 ※ attribute の部分は kind, purpose, place のどれかに読み替えること
 
