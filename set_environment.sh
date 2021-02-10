31dbaa226cdf7b9d29cdf48b8b38c2797b6dfc75
firedial <42674772+firedial@users.noreply.github.com>
2021-02-11 00:39:32 +0900

    misuzu の設定

--- set_environment.sh
+++ set_environment.sh
@@ -10,4 +10,10 @@ sed -i '' -e "s|API_URL:.*|API_URL: '\"$API_URL\"'|" ./midas-eru/config/dev.env.
 cp ./midas-nagisa/src/Config/Env.elm.sample ./midas-nagisa/src/Config/Env.elm
 sed -i '' -e "s|getApiUrl = .*|getApiUrl = \"$API_URL\"|" ./midas-nagisa/src/Config/Env.elm
 
+# misuzu
+cp ./midas-misuzu/config/env.go.sample ./midas-misuzu/config/env.go
+sed -i '' -e "s|const DB_HOST.*|const DB_HOST = \"$DB_HOST\"|" ./midas-misuzu/config/env.go
+sed -i '' -e "s|const DB_USER.*|const DB_USER = \"$DB_CR_USER\"|" ./midas-misuzu/config/env.go
+sed -i '' -e "s|const DB_PASSWORD.*|const DB_PASSWORD = \"$DB_CR_PASS\"|" ./midas-misuzu/config/env.go
+sed -i '' -e "s|const IS_PRODUCTION.*|const IS_PRODUCTION = $IS_PRODUCTION|" ./midas-misuzu/config/env.go
 
