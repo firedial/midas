c27637d0c7b09330420d9a777d39e246adddc50a
firedial <42674772+firedial@users.noreply.github.com>
2021-04-04 14:24:03 +0900

    dockerfileからなぜかできないのでこっちに移す

--- midas-rikka/init.sh
+++ midas-rikka/init.sh
@@ -1,6 +1,17 @@
+#!/bin/bash
+
+apk --update add tzdata
+cp /usr/share/zoneinfo/Asia/Tokyo /etc/localtime
+apk del tzdata
+rm -rf /var/cache/apk/* 
+
+apk add mariadb-dev
+apk add mariadb-client
+
+apk add curl
+
 mount -t cifs ${NAS_PATH} /mnt/nas -o username=${NAS_USER},password=${NAS_PASS},iocharset=utf8,rw,uid=0,gid=0
 
 /usr/sbin/crond
 crontab /home/root/crontab_setting
-
 
